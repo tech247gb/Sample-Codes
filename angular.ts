2e1ce68e449dc749874c7af401b102ab7c59bbaa
tech247gb <54674453+tech247gb@users.noreply.github.com>
2023-05-25 17:29:21 +0530

    Add files via upload

---
+++ angular.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import {environment} from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployessService {
+  /**create behaviourSubject object */
+  private employeeListSource = new BehaviorSubject('');
+  /**asssign list current behaviourSubject object */
+  currentEmployeeList = this.employeeListSource.asObservable();
+
+  constructor(  private http: HttpClient) { }
+/**get data list from api */
+  getEmployees(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      return this.http.get(environment.globalURL+'users').subscribe((response: any) => {
+        resolve(response);
+      }, reject);
+    });
+  }
+  /**get one data  from api */
+  getEmployee(id:any): Promise<any> {
+    return new Promise((resolve, reject) => {
+      return this.http.get(environment.globalURL+'users/'+id).subscribe((response: any) => {
+        resolve(response);
+      }, reject);
+    });
+  }
+  /**add data to backend */
+  addEmployee(data:any): Promise<any> {
+    return new Promise((resolve, reject) => {
+      return this.http.post(environment.globalURL+'users',data).subscribe((response: any) => {
+        resolve(response);
+      }, reject);
+    });
+  }
+  /**Edit data and pass to backend */
+  editEmployee(data:any,item:any): Promise<any> {
+    return new Promise((resolve, reject) => {
+      return this.http.put(environment.globalURL+'users/'+item.id,data).subscribe((response: any) => {
+        resolve(response);
+      }, reject);
+    });
+  }
+  /**Delete data */
+  deleteEmployee(item:any): Promise<any> {
+    return new Promise((resolve, reject) => {
+      return this.http.delete(environment.globalURL+'users/'+item.id).subscribe((response: any) => {
+        resolve(response);
+      }, reject);
+    });
+  }
+  /**change data list without reload page */
+  changeList(item:any) {
+    this.employeeListSource.next(item);
+  }
+}
